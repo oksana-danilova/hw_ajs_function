eceacc052f57826baa18c4f1d97b6eb2e73aa143
Oksana Danilova <ilimlaw38@gmail.com>
2024-10-29 18:22:05 +0800

    initiak commit

---
+++ src/js/__tests__/test.js
@@ -0,0 +1,22 @@
+import showLifeBar from "../lifebar";
+
+test("testing show lifebar with healthy status", () => {
+  const heroObj = { name: "Маг", health: 90 };
+
+  const result = showLifeBar(heroObj);
+  expect(result).toBe("healthy");
+});
+
+test("testing show lifebar with wounded status", () => {
+  const heroObj = { name: "Наг", health: 20 };
+
+  const result = showLifeBar(heroObj);
+  expect(result).toBe("wounded");
+});
+
+test("testing show lifebar with critical status", () => {
+  const heroObj = { name: "Мак", health: 5 };
+
+  const result = showLifeBar(heroObj);
+  expect(result).toBe("critical");
+});
---
+++ src/js/lifebar.js
@@ -0,0 +1,11 @@
+export default function showLifeBar(obj) {
+    let result = "";
+    if (obj.health >= 50) {
+      result = "healthy";
+    } else if (obj.health >= 15) {
+      result = "wounded";
+    } else if (obj.health < 15) {
+      result = "critical";
+    }
+    return result;
+  }
